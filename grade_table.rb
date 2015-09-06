ff48374efd737c641fe458d402402175eef382cf
Brian Isganitis <brianji03@gmail.com>
2015-09-06 00:57:09 -0400

    Added support for header.

--- grade_table.rb
+++ grade_table.rb
@@ -2,15 +2,21 @@
 
 require 'csv'
 
-csv_file = CSV.read(ARGV[0])
+csv_file = CSV.read ARGV[0]
 table_name = ARGV[0].split(".")[0]
 
 # Start table
 puts "<h2>#{table_name.upcase}</h2>"
 puts '<table border="1px solid black">'
 
+print "<tr>"
+csv_file[0].each { |col|
+	print "<th>#{col.strip}</th>"
+}
+puts "</tr>"
+
 # Table contents
-csv_file.each { |row|
+csv_file[1...csv_file.length].each { |row|
 	print "<tr>"
 	row.each { |col|
 		print "<td>#{col.strip}</td>"
