d9d8522e5be57f5ef4e5fe0f2d507b61bddb55f2
Brian Isganitis <brianji03@gmail.com>
2015-09-06 16:43:47 -0400

    Exit if no filename provided.

--- grade_table.rb
+++ grade_table.rb
@@ -2,28 +2,24 @@
 
 require 'csv'
 
-csv_file = CSV.read ARGV[0]
-table_name = ARGV[0].split(".")[0]
+# Exit if no file provided
+if ARGV.length == 0 then abort "Usage: #{$0} ASSIGNMENT_NAME.csv" end
 
-# Start table
-puts "<h2>#{table_name.upcase}</h2>"
+csv_file = CSV.read ARGV[0]
+
+puts "<h2>#{ARGV[0].split(".")[0].upcase}</h2>" # Title
 puts '<table border="1px solid black">'
 
 # Table header
 print "<tr>"
-csv_file[0].each { |col|
-  print "<th>#{col.strip}</th>"
-}
+csv_file[0].each { |col| print "<th>#{col.strip}</th>" }
 puts "</tr>"
 
 # Table contents
 csv_file[1...csv_file.length].each { |row|
-  print "<tr>"
-  row.each { |col|
-    print "<td>#{col.strip}</td>"
-  }
-  puts "</tr>"
+	print "<tr>"
+	row.each { |col| print "<td>#{col.strip}</td>" }
+	puts "</tr>"
 }
 
-# End table
 puts "</table>"
